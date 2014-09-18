1c90846a6aab16d43b52ea5e76bd887ddddedd04
Hudson <Hudson@Alpha>
2014-09-18 20:17:25 -0300

    Lazy Singleton 2, 3

---
+++ src/br/com/schumaker/gof/creational/singleton/LazySingleton3.java
@@ -0,0 +1,23 @@
+package br.com.schumaker.gof.creational.singleton;
+
+/**
+ * @author Hudson Schumaker
+ * @version 1.0.0
+ * @since 10/09/2014
+ */
+public class LazySingleton3 {
+
+    private volatile static LazySingleton3 instance = null;
+
+    private LazySingleton3() {
+    }
+
+    public static LazySingleton3 getInstance() {
+        if (instance == null) {
+            synchronized (getInstance()) {
+                instance = new LazySingleton3();
+            }
+        }
+        return instance;
+    }
+}
