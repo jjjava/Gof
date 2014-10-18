7dd7dcc1caee04eae52cf423d48b8b7dbd804242
Hudson <Hudson@Alpha>
2014-10-18 14:59:26 -0300

    NoGof - NullObject  - Example3 - Bubble Sort

--- src/br/com/schumaker/nogof/behavioral/nullobject/example3/HsBubbleSort.java
+++ src/br/com/schumaker/nogof/behavioral/nullobject/example3/HsBubbleSort.java
@@ -20,13 +20,11 @@ public class HsBubbleSort implements SortList {
 
     private void bubbleSort(List<Integer> list) {
         boolean sorted = false;
-
         for (int top = list.size() - 1; top > 0 && !sorted; top--) {
             sorted = true;
             for (int i = 0; i < top; i++) {
                 if (list.get(i) > list.get(i + 1)) {
                     sorted = false;
-
                     int temp = list.get(i);
                     list.add(i, list.get(i + 1));
                     list.add(i + 1, temp);
