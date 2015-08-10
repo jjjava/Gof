e03283c6275f25ad20f72d05b5644697f6059f6d
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2015-08-10 13:36:38 -0300

    Adjust Virtual Proxy

--- src/br/com/schumaker/gof/structural/proxy/virtualproxy/example1/Client.java
+++ src/br/com/schumaker/gof/structural/proxy/virtualproxy/example1/Client.java
@@ -15,10 +15,14 @@ public class Client {
         System.out.println(new ProxyFactorial(25).getResult());
         // first time ask 10!, calculation required
         System.out.println(new ProxyFactorial(10).getResult());
-         System.out.println(new ProxyFactorial(999999999).getResult());
+        System.out.println(new ProxyFactorial(999999999).getResult());
+        System.out.println(new ProxyFactorial(999999998).getResult());
+        System.out.println(new ProxyFactorial(999999997).getResult());
         // second time, do not need to calculate
         System.out.println(new ProxyFactorial(10).getResult());
         System.out.println(new ProxyFactorial(25).getResult());
-         System.out.println(new ProxyFactorial(999999999).getResult());
+        System.out.println(new ProxyFactorial(999999999).getResult());
+        System.out.println(new ProxyFactorial(999999998).getResult());
+        System.out.println(new ProxyFactorial(999999997).getResult());
     }
 }
