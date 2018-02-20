b0866888952fafcf83b20636c607f846dc95d3db
Hudson Schumaker <hudson.schumaker@provider-it.com.br>
2018-02-20 15:35:53 -0300

    :: chamando meu metodo.

---
+++ src/br/com/schumaker/gof/creational/builder/example1/FazNada.java
@@ -0,0 +1,13 @@
+
+package br.com.schumaker.gof.creational.builder.example1;
+
+/**
+ *
+ * @author hudson.schumaker
+ */
+public class FazNada {
+
+    public static void imprimi(Integer i) {
+        System.out.println(i);
+    }
+}
--- src/br/com/schumaker/gof/creational/builder/example1/Run.java
+++ src/br/com/schumaker/gof/creational/builder/example1/Run.java
@@ -1,5 +1,6 @@
 package br.com.schumaker.gof.creational.builder.example1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,12 +12,17 @@ public class Run {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(3, 4, 2, 1, 5);
-        List<String> letras = Arrays.asList("b", "a","c", "d", "e");
-
+        List<String> letras = Arrays.asList("b", "a", "c", "d", "e");
+        List<String> n = Arrays.asList("3", "4", "2", "1", "5");
         numbers.forEach(System.out::println);
 
-        letras.stream().map(String::toUpperCase).sorted()
-                .forEach(System.out::println);
+        letras.stream().map(String::toUpperCase).sorted().forEach(System.out::println);
 
+        List<String> conv = new ArrayList<>();
+
+        n.stream().map(Integer::parseInt).sorted().forEach(System.out::println);
+
+        Arrays.asList("3", "4", "2", "1", "5").stream().map(Integer::parseInt)
+                .sorted().forEach(FazNada::imprimi);
     }
 }
